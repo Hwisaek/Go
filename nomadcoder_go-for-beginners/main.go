1b94c9f98eb43911ac0168c0e5fcebdfdbd7df6b
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-12-12 13:13:36 +0900

    Update main.go

--- nomadcoder_go-for-beginners/main.go
+++ nomadcoder_go-for-beginners/main.go
@@ -1,17 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	length, uppercase = len(name), strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("hwisaek")
-	fmt.Println(totalLength)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	println(result)
 }
