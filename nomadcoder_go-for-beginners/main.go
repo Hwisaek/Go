e4cf095a1cfd106914ef6e9854328924fe465a31
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-12-26 16:07:38 +0900

    Update main.go

--- nomadcoder_go-for-beginners/main.go
+++ nomadcoder_go-for-beginners/main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/Hwisaek/Go/nomadcoder_go-for-beginners/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico, nico.name)
+	account := accounts.NewAccount("nico")
+	fmt.Println(account)
 }
