fb2e739e71c56f607f935565f4ff32c347921acd
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-12-26 20:39:48 +0900

    #2.6 Update Delete

--- nomadcoder_go-for-beginners/mydict/mydict.go
+++ nomadcoder_go-for-beginners/mydict/mydict.go
@@ -5,8 +5,12 @@ import "errors"
 // Dictioinary type
 type Dictioinary map[string]string
 
-var errNotFound = errors.New("not Found")
-var errWordExists = errors.New("that word already exists")
+var (
+	errNotFound   = errors.New("not Found")
+	errCantUpdate = errors.New("can't update non-existing word")
+	errCantDelete = errors.New("can't delete non-existing word")
+	errWordExists = errors.New("that word already exists")
+)
 
 // Saerch for a word
 func (d Dictioinary) Search(word string) (string, error) {
@@ -25,6 +29,30 @@ func (d Dictioinary) Add(word, def string) error {
 		d[word] = def
 	case nil:
 		return errWordExists
+	}
+	return nil
+}
+
+// Update a word to the dictionary
+func (d Dictioinary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word to the dictionary
+func (d Dictioinary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCantDelete
 	}
 	return nil
 }
