3eb2f0b5c326a21a220ccf2694965390ddc8e1d7
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-12-26 21:51:20 +0900

    #3.6 URLChecker + Go Routines

--- nomadcoder_go-for-beginners/main.go
+++ nomadcoder_go-for-beginners/main.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+type result struct {
+	url    string
+	status string
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 2)
-	c <- person + " is sexy"
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	results := make(map[string]string)
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking:", url)
+
+	resp, err := http.Get(url)
+	status := "OK"
+
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+
+	c <- result{url: url, status: status}
 }
