d2c4ceee2a3f27fcb166756d13b7530f00d68e99
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2021-12-26 15:38:04 +0900

    Update main.go

--- nomadcoder_go-for-beginners/main.go
+++ nomadcoder_go-for-beginners/main.go
@@ -2,16 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	}
-}
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a
+	a = 5
+	fmt.Println(a, *b)
 }
