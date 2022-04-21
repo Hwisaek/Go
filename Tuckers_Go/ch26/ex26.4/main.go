9ec039f76e113fff5c44d104b18ad98f59918434
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-04-22 01:09:11 +0900

    26장

---
+++ Tuckers_Go/ch26/ex26.4/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	fileName string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행 인수가 필요합니다. ex) ex26.3 word filepath")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.fileName)
+		fmt.Println("-----------------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("-----------------------------------")
+		fmt.Println()
+	}
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	var findInfos []FindInfo
+
+	fileList, err := GetFileList(path)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. err: ", err)
+		return findInfos
+	}
+
+	ch := make(chan FindInfo)
+	cnt := len(fileList)
+	recvCnt := 0
+
+	for _, fileName := range fileList {
+		go FindWordInFile(word, fileName, ch)
+	}
+
+	for findInfo := range ch {
+		findInfos = append(findInfos, findInfo)
+		recvCnt++
+		if recvCnt == cnt {
+			// all received
+			break
+		}
+	}
+	return findInfos
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+
+func FindWordInFile(word, fileName string, ch chan FindInfo) {
+	findInfo := FindInfo{fileName, []LineInfo{}}
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. ", fileName)
+		ch <- findInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	ch <- findInfo
+}
