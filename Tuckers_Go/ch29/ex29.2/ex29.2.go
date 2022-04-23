d77345fb2ba0d121df8b9c49a7e56a50fada7bc0
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-04-23 22:59:13 +0900

    29.1~4

---
+++ Tuckers_Go/ch29/ex29.2/ex29.2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/bar", barHandler)
+
+	http.ListenAndServe(":3000", nil)
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	name := values.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	id, _ := strconv.Atoi(values.Get("id"))
+	fmt.Fprintf(w, "Hello %s! id:%d", name, id)
+}
